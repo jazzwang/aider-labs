cc4157726d78423a8a15050bf83320e41eb6db3a
jazzwang <jazzwang.tw@gmail.com>
2025-07-06 22:46:57 +0800

    --Restore lab3/init-lab3.sh

---
+++ lab3/init-lab3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! which ollama > /dev/null; then                ## save some time if ollama was already installed
+  echo "Installing Ollama ..."
+  curl -fsSL https://ollama.com/install.sh | sh
+fi
+echo "Starting Ollama Server  ..."
+ollama start &
+sleep 10
+echo "Pulling Gemma 3 1B model file ..."
+ollama pull gemma3:1b
+if ! which aider > /dev/null; then                ## save some time if aider was already installed
+  echo "Installing Aider (using pip) ..."
+  python -m pip install aider-install
+  aider-install
+fi
+aider --version
