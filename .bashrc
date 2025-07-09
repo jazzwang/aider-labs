102c327b368fc885e4f739d6a54b72f16e258935
jazzwang <jazzwang.tw@gmail.com>
2025-07-09 12:30:10 +0800

    feat: [lab10] shell script to reproduce convert bash to python.

--- .bashrc
+++ .bashrc
@@ -1,2 +1,2 @@
-export OLLAMA_API_BASE=http://127.0.0.1:11434
-export AIDER_EDITOR=code --wait
+export OLLAMA_API_BASE="http://127.0.0.1:11434"
+export AIDER_EDITOR="code --wait"
--- lab3/init-lab3.sh
+++ lab3/init-lab3.sh
@@ -9,6 +9,7 @@ ollama start &
 sleep 10
 echo "Pulling Gemma 3 1B model file ..."
 ollama pull gemma3:1b
+ollama pull qwen2.5-coder
 if ! which aider > /dev/null; then                ## save some time if aider was already installed
   echo "Installing Aider (using pip) ..."
   python -m pip install aider-install
